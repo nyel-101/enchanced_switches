0dbe6011e4dad69c451a4b4c2ca725d40af522bb
nyel-101 <dandevjr@gmail.com>
2025-02-14 20:52:08 +0000

    perf(deps): fix caching layer

--- fury/src/controllers/theme.js
+++ fury/src/controllers/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fffe205d473a40f1a2206572ee5d5fe4 = () => {
-  return '2026-05-07T04:44:27Z';
+const node_b39f4e4bcb3a4cb2b3e6ae37d68e683a = () => {
+  return '2025-02-14T20:52:08Z';
 };
