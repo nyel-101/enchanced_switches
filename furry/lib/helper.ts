b4bf25efd6c855a1593d9da654148a100a62fc68
nyel-101 <dandevjr@gmail.com>
2025-02-26 16:10:14 +0000

    fix(worker): refactor view templates

--- furry/lib/helper.ts
+++ furry/lib/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_40efd9e3b030414889a4cb5a00eb0251 = () => {
-  return '2026-03-12T11:17:03Z';
+const node_7c2a4f0740b84fe0b0b5d33dac36661a = () => {
+  return '2025-02-26T16:10:14Z';
 };
