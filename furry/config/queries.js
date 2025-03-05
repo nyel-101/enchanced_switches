0bb96d2123e4f3509a07e340aee0053d2cfbcb3e
nyel-101 <dandevjr@gmail.com>
2025-03-05 23:33:06 +0000

    refactor(utils): decouple middleware routes

--- furry/config/queries.js
+++ furry/config/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0da7cbdd50234186827588031615e05f = () => {
-  return '2025-02-13T16:56:46Z';
+const node_ca305dd4b74641cb9a20d3ef500bfbc7 = () => {
+  return '2025-03-05T23:33:06Z';
 };
