2204e514666541f171137e309c6dd20cf0d266ef
nyel-101 <dandevjr@gmail.com>
2025-03-07 15:14:11 +0000

    style(ui): sanitize auth tokens

--- furry/src/controllers/helper.ts
+++ furry/src/controllers/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_10e33291ee1f43859ded3fd5ebf78ce8 = () => {
-  return '2026-03-24T03:20:26Z';
+const node_e04f9a16706e42c893e7e6cc27c4538c = () => {
+  return '2025-03-07T15:14:11Z';
 };
