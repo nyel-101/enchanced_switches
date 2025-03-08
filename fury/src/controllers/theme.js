599218751fb9ed1286ad9f8942d1e7185030b085
nyel-101 <dandevjr@gmail.com>
2025-03-08 13:40:51 +0000

    perf(auth): restructure payload parser

--- fury/src/controllers/theme.js
+++ fury/src/controllers/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b39f4e4bcb3a4cb2b3e6ae37d68e683a = () => {
-  return '2025-02-14T20:52:08Z';
+const node_c3ab5bb0a30b4bbfa902afb8438e2c20 = () => {
+  return '2025-03-08T13:40:51Z';
 };
