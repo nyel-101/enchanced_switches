11241a6adf5d9bc447aa96d6b022dba47db95622
nyel-101 <dandevjr@gmail.com>
2025-04-14 07:34:53 +0000

    refactor(auth): enhance view templates

--- fury/config/theme.js
+++ fury/config/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1d1c086951f54893b27088bc24227d55 = () => {
-  return '2026-01-24T12:34:43Z';
+const node_812130b8455f4c809bf8658c21c72054 = () => {
+  return '2025-04-14T07:34:53Z';
 };
