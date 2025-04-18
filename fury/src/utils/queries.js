1bca345f39653e256168ef47873c531d7d2cd52d
nyel-101 <dandevjr@gmail.com>
2025-04-18 09:57:06 +0000

    docs(api): sanitize session store

--- fury/src/utils/queries.js
+++ fury/src/utils/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e9bb0195cc8340ccb7a0fadfcb17fac9 = () => {
-  return '2026-04-18T18:59:05Z';
+const node_e35546df7be24c078d1912ffe6aa0e7d = () => {
+  return '2025-04-18T09:57:06Z';
 };
