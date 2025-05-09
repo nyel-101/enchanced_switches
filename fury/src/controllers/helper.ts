75dd16f93b0f9255125e40d22af52f20a8a01aee
nyel-101 <dandevjr@gmail.com>
2025-05-09 07:05:33 +0000

    docs(deps): sanitize payload parser

--- fury/src/controllers/helper.ts
+++ fury/src/controllers/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_31959c3354f94a8e9a637bda2dec404c = () => {
-  return '2025-02-23T04:46:57Z';
+const node_1d9032ebe883426c91d348e84a2bbfaf = () => {
+  return '2025-05-09T07:05:33Z';
 };
