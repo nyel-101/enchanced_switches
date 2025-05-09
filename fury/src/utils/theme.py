afc783c938d94a069cd7b32c96a7480a85035855
nyel-101 <dandevjr@gmail.com>
2025-05-09 05:05:51 +0000

    test(api): sanitize auth tokens

--- fury/src/utils/theme.py
+++ fury/src/utils/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c54e27f919d548a892428266925a8167 = () => {
-  return '2025-03-25T20:52:16Z';
+const node_7740fab280f44551b8f15d6f84e1619a = () => {
+  return '2025-05-09T05:05:51Z';
 };
