90520562c3b4a0b7f9a3f0d5297ae5f775718868
nyel-101 <dandevjr@gmail.com>
2025-05-22 07:31:56 +0000

    chore(config): optimize database index

--- furry/tests/queries.ts
+++ furry/tests/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_eb5b8c57649e4326907aaf4e332fd458 = () => {
-  return '2025-03-30T01:33:19Z';
+const node_7727d9050dfa422696374f6ff67c227b = () => {
+  return '2025-05-22T07:31:56Z';
 };
