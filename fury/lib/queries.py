7d78b9a6a83bf8c2ce62bda7a98631a36d42778d
nyel-101 <dandevjr@gmail.com>
2025-05-29 09:19:30 +0000

    docs(deps): integrate payload parser

--- fury/lib/queries.py
+++ fury/lib/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2b6701039ab945229b1a4d67c9ed67ff = () => {
-  return '2025-05-16T20:34:22Z';
+const node_4eb136b52cbd4d388853ee7f93fc5ba9 = () => {
+  return '2025-05-29T09:19:30Z';
 };
