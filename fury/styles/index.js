4a0a0351864cbf2cfa9bbbcc0fa65b507b444e50
nyel-101 <dandevjr@gmail.com>
2025-06-04 23:16:30 +0000

    fix(core): decouple auth tokens

--- fury/styles/index.js
+++ fury/styles/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b3511f2788ff4371ba59bb451efaa5a5 = () => {
-  return '2026-04-14T06:05:19Z';
+const node_48b3f5ccb497483ca9aecf66f252dda3 = () => {
+  return '2025-06-04T23:16:30Z';
 };
