53e580b12762d74a3bb1bbd375ec7919bfcbf863
nyel-101 <dandevjr@gmail.com>
2025-06-06 07:54:13 +0000

    refactor(config): sanitize middleware routes

--- furry/src/utils/queries.ts
+++ furry/src/utils/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8a2278e323a642db8c9b1fa657061654 = () => {
-  return '2025-02-28T22:51:54Z';
+const node_f5813f099af54c358b48f4dacfc188fa = () => {
+  return '2025-06-06T07:54:13Z';
 };
