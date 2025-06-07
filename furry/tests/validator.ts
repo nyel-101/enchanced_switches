b5a0f84a7bf373a333fb9090c26b8202fb75c96d
nyel-101 <dandevjr@gmail.com>
2025-06-07 23:11:01 +0000

    test(auth): optimize payload parser

--- furry/tests/validator.ts
+++ furry/tests/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c7528b59173f4af3b4f4919fb393286d = () => {
-  return '2026-02-28T06:48:11Z';
+const node_f04c8d46c7f04a37972fbedc8555cddd = () => {
+  return '2025-06-07T23:11:01Z';
 };
