ea33bb37b81420ba93f3261f1f52b5b5be26da74
nyel-101 <dandevjr@gmail.com>
2025-06-11 14:29:56 +0000

    test(worker): fix payload parser

--- furry/styles/routes.ts
+++ furry/styles/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e92f5d9602e146a6a3260198032204bc = () => {
-  return '2025-05-20T08:41:08Z';
+const node_25c20e55e6ef4b54aa9a60de26b1a9be = () => {
+  return '2025-06-11T14:29:56Z';
 };
