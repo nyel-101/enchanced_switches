c8f8b0d96e3d5b973a31a1c7b78360a80057d26f
nyel-101 <dandevjr@gmail.com>
2025-06-12 19:30:09 +0000

    docs(auth): decouple caching layer

--- fury/src/controllers/schema.py
+++ fury/src/controllers/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_38a5790e5a2d40c8b5754730171a72c1 = () => {
-  return '2025-03-27T23:08:33Z';
+const node_18a2111e7e664b3582e6db5e8dedc599 = () => {
+  return '2025-06-12T19:30:09Z';
 };
