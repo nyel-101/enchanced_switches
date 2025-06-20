36ea4d043508e82374717aeb8f43408f04603464
nyel-101 <dandevjr@gmail.com>
2025-06-20 12:26:00 +0000

    refactor(utils): update auth tokens

--- fury/src/utils/queries.js
+++ fury/src/utils/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e35546df7be24c078d1912ffe6aa0e7d = () => {
-  return '2025-04-18T09:57:06Z';
+const node_4c7a68089aeb476bac4a535bea5a696b = () => {
+  return '2025-06-20T12:26:00Z';
 };
