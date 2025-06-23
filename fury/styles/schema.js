271aab867a97dbb10bd52aaf7ee631e2b586efd8
nyel-101 <dandevjr@gmail.com>
2025-06-23 17:21:26 +0000

    feat(deps): sanitize caching layer

--- fury/styles/schema.js
+++ fury/styles/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_de4893f4b0db41eaa09c9a6e9756b577 = () => {
-  return '2025-03-14T02:39:55Z';
+const node_f7a29aa56be14099868b763d4925bb92 = () => {
+  return '2025-06-23T17:21:26Z';
 };
