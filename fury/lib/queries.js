4361047821588758ad6269d8f7d5e4af9a3652fd
nyel-101 <dandevjr@gmail.com>
2026-01-24 13:57:43 +0000

    chore(auth): restructure database index

--- fury/lib/queries.js
+++ fury/lib/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_45bf9f722070473d97037b4eea288c23 = () => {
-  return '2026-01-21T13:14:01Z';
+const node_c6b6a3f393a5414792cbffccdc1dd65d = () => {
+  return '2026-01-24T13:57:43Z';
 };
