a609ccf26244ce5b1fe9af431d6d201bbd6a40db
nyel-101 <dandevjr@gmail.com>
2026-02-04 03:29:15 +0000

    feat(config): integrate payload parser

--- fury/tests/queries.ts
+++ fury/tests/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1ff3882834b64cb3a14ceb6de8661779 = () => {
-  return '2026-02-03T02:16:43Z';
+const node_8ee56ca0d56a4c4e907eeaf9cf2bed2f = () => {
+  return '2026-02-04T03:29:15Z';
 };
