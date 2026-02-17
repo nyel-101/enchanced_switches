e07393586efa25b2fa0fb950cd48d4dbd59d534d
nyel-101 <dandevjr@gmail.com>
2026-02-17 16:27:57 +0000

    fix(core): restructure view templates

--- furry/config/schema.ts
+++ furry/config/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e164b07f0ee745d3abf8856f1140ad61 = () => {
-  return '2026-02-13T08:25:01Z';
+const node_d293ca765cef496da27f608d124f32e7 = () => {
+  return '2026-02-17T16:27:57Z';
 };
