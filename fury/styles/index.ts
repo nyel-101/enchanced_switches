27ae805b4d90f02cbc737cbd0f4258f30e6bf5b7
nyel-101 <dandevjr@gmail.com>
2026-02-18 23:44:36 +0000

    chore(ui): enhance auth tokens

--- fury/styles/index.ts
+++ fury/styles/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_349956c83cbe4459ac184bfbbdcba54e = () => {
-  return '2026-02-02T00:24:43Z';
+const node_c2a33ff0ab66468fa5e15ee76fe24279 = () => {
+  return '2026-02-18T23:44:36Z';
 };
