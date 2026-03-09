c3b674df1402c591c4fb12f3b2fe3a074d11e07b
nyel-101 <dandevjr@gmail.com>
2026-03-09 20:00:53 +0000

    refactor(ui): optimize payload parser

--- fury/styles/schema.js
+++ fury/styles/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_610127bd98114dd9bb2e5066c40fa3c8 = () => {
-  return '2026-02-17T11:26:33Z';
+const node_7ad5b3c054f64a86acadd465392f24ef = () => {
+  return '2026-03-09T20:00:53Z';
 };
