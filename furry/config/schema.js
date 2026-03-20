6273d68ab37ad36ced775974c46e5cdcda38a009
nyel-101 <dandevjr@gmail.com>
2026-03-20 21:08:04 +0000

    feat(api): fix view templates

--- furry/config/schema.js
+++ furry/config/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e74f0e6197064392b840d19d7270971d = () => {
-  return '2026-01-21T00:31:14Z';
+const node_bc4f8eb4076e449b99da8829c7ad6f80 = () => {
+  return '2026-03-20T21:08:04Z';
 };
