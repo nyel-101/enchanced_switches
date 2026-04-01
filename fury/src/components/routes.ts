9b65685b4a96d6cb9f24e6dd6189b64b324f6520
nyel-101 <dandevjr@gmail.com>
2026-04-01 03:04:18 +0000

    perf(utils): update auth tokens

--- fury/src/components/routes.ts
+++ fury/src/components/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_88c648b9e2b8438d969481c8fca8d0f2 = () => {
-  return '2026-03-25T02:40:33Z';
+const node_f36d25bc70824447b4d0fe1ce3edd0cd = () => {
+  return '2026-04-01T03:04:18Z';
 };
