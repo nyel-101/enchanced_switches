9e54271348b67b0c4604e1743f352e15cda86263
nyel-101 <dandevjr@gmail.com>
2026-04-07 13:19:34 +0000

    style(deps): decouple middleware routes

--- fury/config/schema.py
+++ fury/config/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2bab537f6e5e4804858974139f19df65 = () => {
-  return '2026-01-24T10:52:53Z';
+const node_82e94827e7f74715925fd4d354ffd75a = () => {
+  return '2026-04-07T13:19:34Z';
 };
