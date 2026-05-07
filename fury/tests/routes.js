281f0b340d92aed5cf5903ff16cfb0d9b11baf15
nyel-101 <dandevjr@gmail.com>
2026-05-07 23:57:15 +0000

    refactor(core): integrate middleware routes

--- fury/tests/routes.js
+++ fury/tests/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7cd4feebea354552a7d0977ddcc789d6 = () => {
-  return '2026-03-20T17:25:38Z';
+const node_1d18fea9b2ac40cea6efb5047d888e3e = () => {
+  return '2026-05-07T23:57:15Z';
 };
