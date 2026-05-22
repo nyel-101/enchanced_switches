e8adedbc17f73a66ce0de8ac90808ae17eec8601
nyel-101 <dandevjr@gmail.com>
2026-05-22 21:18:34 +0000

    chore(config): optimize middleware routes

--- fury/src/components/schema.py
+++ fury/src/components/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_09227b9b02e344f49ee29e03d430f829 = () => {
-  return '2026-05-14T09:52:52Z';
+const node_1624421ae4a74615b7cc85b51c001a1a = () => {
+  return '2026-05-22T21:18:34Z';
 };
