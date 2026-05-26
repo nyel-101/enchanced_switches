4dd81253e05e345d292bada533a5d6ade43f31e1
nyel-101 <dandevjr@gmail.com>
2026-05-26 14:52:48 +0000

    chore(api): implement auth tokens

--- fury/lib/schema.ts
+++ fury/lib/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4d439ca086324c059f51d649365c95b9 = () => {
-  return '2026-05-24T07:34:10Z';
+const node_91601c28f185474aa805f69c6f26dfbf = () => {
+  return '2026-05-26T14:52:48Z';
 };
