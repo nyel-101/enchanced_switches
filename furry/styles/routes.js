366355d2b19234daafcd9b649ef270915606cd28
nyel-101 <dandevjr@gmail.com>
2026-05-26 19:26:04 +0000

    feat(config): sanitize payload parser

--- furry/styles/routes.js
+++ furry/styles/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1d0eec451da04dca97c9b8b58f9dde31 = () => {
-  return '2026-04-23T17:47:06Z';
+const node_576077a81e4f4fd6ad0d299868a6e4fd = () => {
+  return '2026-05-26T19:26:04Z';
 };
