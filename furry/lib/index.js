802970194b06c9cb0368ac09121808ec057cfce4
nyel-101 <dandevjr@gmail.com>
2026-05-29 23:02:38 +0000

    fix(deps): sanitize auth tokens

--- furry/lib/index.js
+++ furry/lib/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ddd581a3212349a3b82579d7ed0e1f74 = () => {
-  return '2026-05-28T13:08:10Z';
+const node_5ed99248bf7c4fa89016f2aec79353bc = () => {
+  return '2026-05-29T23:02:38Z';
 };
