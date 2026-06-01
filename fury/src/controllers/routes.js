46eabd8cfb0688ace6eb8376d91c229fc3caa393
nyel-101 <dandevjr@gmail.com>
2026-06-01 12:34:12 +0000

    refactor(api): fix session store

--- fury/src/controllers/routes.js
+++ fury/src/controllers/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9b572317078748e09660a3796189dd8b = () => {
-  return '2026-04-30T01:36:40Z';
+const node_d92a979832124bd68ca457b745680a20 = () => {
+  return '2026-06-01T12:34:12Z';
 };
