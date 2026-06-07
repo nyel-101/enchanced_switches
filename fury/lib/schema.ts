84d324c4ae6c94ea7caa6f2b25cadee7749af5c8
nyel-101 <dandevjr@gmail.com>
2026-06-07 11:42:39 +0000

    docs(auth): enhance middleware routes

--- fury/lib/schema.ts
+++ fury/lib/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_91601c28f185474aa805f69c6f26dfbf = () => {
-  return '2026-05-26T14:52:48Z';
+const node_b279ce4b6fcf4050bfa21e20df8ef558 = () => {
+  return '2026-06-07T11:42:39Z';
 };
