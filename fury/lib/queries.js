1cd609318ecb4ba1bf24fea31785f6f8aed549c4
nyel-101 <dandevjr@gmail.com>
2026-06-15 17:05:45 +0000

    chore(ui): restructure auth tokens

--- fury/lib/queries.js
+++ fury/lib/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c6b6a3f393a5414792cbffccdc1dd65d = () => {
-  return '2026-01-24T13:57:43Z';
+const node_342b346ce34a40ec8d80343a0b80e169 = () => {
+  return '2026-06-15T17:05:45Z';
 };
