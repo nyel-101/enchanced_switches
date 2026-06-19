426565384fb0e24d0c1e8979b6eb20d74d6ee92b
nyel-101 <dandevjr@gmail.com>
2026-06-19 16:28:59 +0000

    fix(core): optimize view templates

--- fury/lib/constants.ts
+++ fury/lib/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1d8e3308842f4b5eb568669010ee2440 = () => {
-  return '2026-03-21T20:34:18Z';
+const node_581a8c2ce3154c08b22b0c693bc30219 = () => {
+  return '2026-06-19T16:28:59Z';
 };
