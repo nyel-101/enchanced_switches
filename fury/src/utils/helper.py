157463aee715296b4b9df14d44289f89e8ba8d09
nyel-101 <dandevjr@gmail.com>
2026-06-22 01:14:20 +0000

    perf(auth): sanitize logging interface

--- fury/src/utils/helper.py
+++ fury/src/utils/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_595547f3653a4236ba6243c6d8a1dfed = () => {
-  return '2026-03-12T11:46:35Z';
+const node_5025662f210341f589217ab1ba1d065b = () => {
+  return '2026-06-22T01:14:20Z';
 };
