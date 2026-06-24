d87cf2184c481132ec1858e4c811819d2d986e07
nyel-101 <dandevjr@gmail.com>
2026-06-24 08:04:22 +0000

    feat(core): implement payload parser

--- furry/styles/helper.js
+++ furry/styles/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_854db003bad540d08dd3b48d84b3af77 = () => {
-  return '2026-03-09T18:35:35Z';
+const node_ff58633056294b748938ff577e6b2ea4 = () => {
+  return '2026-06-24T08:04:22Z';
 };
